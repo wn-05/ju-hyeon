db0f5e32313a9a8557f59875279154d96bd464aa
wn-05 <wn0000505@gmail.com>
2025-07-09 17:39:08 +0900

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -129,8 +129,8 @@ jQuery(function ($) {
                 //your configuration goes here
                 easing: 'easeOut',
                 delay: 3000,
-                barColor: '#68c3a3',
-                trackColor: 'rgba(255,255,255,0.2)',
+                barColor: '#f2f2f2',
+                trackColor: '#333',
                 scaleColor: false,
                 lineWidth: 8,
                 size: 140,
