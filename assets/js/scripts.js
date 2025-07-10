e0ede9bec6207a5c7ce6490aeb1a76c6792231e4
wn-05 <wn0000505@gmail.com>
2025-07-10 16:04:22 +0900

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,225 @@
+/*
+Theme Name: IAMX
+Author: Trendy Theme
+Author URL: trendytheme.net
+*/
+
+/*
+    = Preloader
+    = Animated scrolling / Scroll Up
+    = Full Screen Slider
+    = Sticky Menu
+    = Back To Top
+    = Countup
+    = Progress Bar
+    = More skill
+    = Shuffle
+    = Magnific Popup
+    = Vidio auto play
+    = Fit Vids
+    = Google Map
+
+*/
+
+// swiper
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        slidesPerView: 3,
+
+        // centeredSlides: true,
+        //     slidesPerView: 1.5, // 👉 양옆 슬라이드 일부 보이게
+        //     spaceBetween: 30,   // 👉 슬라이드 간 여백
+
+        loop: true, // 무한 반복
+        autoplay: {
+            delay: 3000,   // 3초마다 자동 전환
+            disableOnInteraction: false, // 사용자 조작 후에도 계속 자동 재생
+        },
+        speed: 2000, // 전환 애니메이션 속도 (1초)
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true, // 페이지네이션 클릭 가능하게
+        },
+    });
+});
+
+new WOW({
+
+    mobile: false
+
+}).init();
+
+jQuery(function ($) {
+
+    'use strict';
+
+    // -------------------------------------------------------------
+    // Full Screen Slider
+    // -------------------------------------------------------------
+    (function () {
+        $(".tt-fullHeight").height($(window).height());
+
+        $(window).resize(function () {
+            $(".tt-fullHeight").height($(window).height());
+        });
+
+    }());
+
+
+    // -------------------------------------------------------------
+    // Sticky Menu
+    // -------------------------------------------------------------
+
+    (function () {
+        $('.header').sticky({
+            topSpacing: 0
+        });
+
+        $('body').scrollspy({
+            target: '.navbar-custom',
+            offset: 70
+        })
+    }());
+
+
+
+
+    // -------------------------------------------------------------
+    // Back To Top
+    // -------------------------------------------------------------
+
+    (function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+    }());
+
+
+    // -------------------------------------------------------------
+    // Countup
+    // -------------------------------------------------------------
+    $('.count-wrap').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+    // -------------------------------------------------------------
+    // Progress Bar
+    // -------------------------------------------------------------
+
+    $('.skill-progress').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'), function () {
+                $(this).css('width', $(this).attr('aria-valuenow') + '%');
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+    // -------------------------------------------------------------
+    // More skill
+    // -------------------------------------------------------------
+    $('.more-skill').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $('.chart').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 3000,
+                barColor: '#f2f2f2',
+                trackColor: '#333',
+                scaleColor: false,
+                lineWidth: 8,
+                size: 140,
+                animate: 2000,
+                onStep: function (from, to, percent) {
+                    this.el.children[0].innerHTML = Math.round(percent);
+                }
+
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+
+    // -------------------------------------------------------------
+    // STELLAR FOR BACKGROUND SCROLLING
+    // -------------------------------------------------------------
+
+    $(window).load(function () {
+
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+
+        } else {
+            $.stellar({
+                horizontalScrolling: false,
+                responsive: true
+            });
+        }
+
+    });
+
+
+    // -------------------------------------------------------------
+    // WOW JS
+    // -------------------------------------------------------------
+
+    // (function () {
+
+
+
+    // }());
+
+    // -------------------------------------------------------------
+    // Popup JS
+    // -------------------------------------------------------------
+
+    $(function () {
+        $('#popup01').click(function () {
+            $('#pop01').show();
+        });
+        $('#popup02').click(function () {
+            $('#pop02').show();
+        });
+
+        $('.popup i').click(function () {
+            $('.popup').hide();
+        });
+    });
+
+});
+
+
+
+
+
+
+
