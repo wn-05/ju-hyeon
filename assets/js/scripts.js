4010104046d9d6431a2f17b5f2dbeccca648da32
wn-05 <wn0000505@gmail.com>
2025-07-14 16:20:25 +0900

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -52,10 +52,25 @@ document.addEventListener("DOMContentLoaded", function () {
             el: ".swiper-pagination",
             clickable: true, // 페이지네이션 클릭 가능하게
         },
+
     });
 });
 
 // 스와이퍼 끝
+
+
+ $(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+        e.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+      }
+    });
+  });
+
 
 
 // 마우스
